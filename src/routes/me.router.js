930a580a115ce3f74599d62cbd1c2854fbd6fe55
harsh kaushal <harsh.groot@outlook.com>
2023-01-18 18:54:44 +0530

    removing me api

--- src/routes/me.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const meRouter = express.Router();
-const {httpMeUser} = require("./me.controller");
-
-
-meRouter.post("/", httpMeUser);
-module.exports = meRouter;
